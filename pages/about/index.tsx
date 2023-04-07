035e44fa3976cd0c377c72e578c9a7a3d3808f29
Abhishek Agarwal <abhihsekag03@gmail.com>
2023-04-07 13:59:33 +0530

    Revert "Merge pull request #6 from abhishekag03/flagsmith_card_control"
    This reverts commit 901b303237e3bd097b4c683e18f7d4ebd055cced, reversing
    changes made to accad5d49028fbc83819cccbb7208c3c303f6a56.

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,23 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { useRouter } from "next/router";
-import { useFlags } from "flagsmith/react";
-import flagsmith from "flagsmith/isomorphic";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
-export default function AboutPage() {
-  const router = useRouter();
-
-  const { name, position } = router.query; // reading the information entered by user
-  let prof: string = position as string; // converting profession to string
-
-  const flags = useFlags(["configure_card"], ["profession"]); // only causes re-render if specified flag values / traits change
-
-  flagsmith.setTrait("profession", prof); // setting the "profession" trait as the one selected by user
-
+export default function Home() {
   return (
     <>
       <Head>
@@ -38,7 +26,7 @@ export default function AboutPage() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{" "}
+              By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -128,23 +116,23 @@ export default function AboutPage() {
               with&nbsp;Vercel
             </p>
           </a>
-          {flags.configure_card.enabled ? (
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={inter.className}>
-                Configure <span>-&gt;</span>
-              </h2>
-              <p className={inter.className}>
-                Configure your feature rollout with&nbsp;Flagsmith
-              </p>
-            </a>
-          ) : null}
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Configure <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Configure your feature rollout
+              with&nbsp;Flagsmith
+            </p>
+          </a>
         </div>
       </main>
     </>
-  );
+  )
 }
